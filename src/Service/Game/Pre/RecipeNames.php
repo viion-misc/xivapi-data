e4cc97f6f456f9b41e7f02ce660f76ff556277ca
Josh Freeman <josh.freeman@sykescottages.co.uk>
2018-09-12 12:15:09 +0100

    Add StainHex Code and Recipe Names

---
+++ src/Service/Game/Pre/RecipeNames.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Service\Game\Pre;
+
+use App\Service\Game\GameData;
+use App\Service\Github\SaintCoinach;
+use App\Service\Tools\Console;
+use App\Service\Tools\Tools;
+
+/**
+ * Copy RewardItem icon to the Recipe so there is an icon at base level
+ */
+class RecipeNames
+{
+    const ENABLED = false;
+    const ORDER = 100;
+
+    public function handle()
+    {
+        $document = GameData::loadDocument('Recipe');
+        $itemDocument = GameData::loadDocument('Item');
+
+        // convert for easy access
+        $itemDocument = GameData::getDocumentsByField($itemDocument, 'ID');
+
+        // add columns
+        GameData::addColumn($document, 'Name_en', 'string');
+        GameData::addColumn($document, 'Name_de', 'string');
+        GameData::addColumn($document, 'Name_fr', 'string');
+        GameData::addColumn($document, 'Name_ja', 'string');
+
+        foreach ($document->Documents as $recipe) {
+            /** @var \stdClass $item */
+            $item = $itemDocument[$recipe->ItemResult] ?? false;
+
+            if (!$item) {
+                // should this ever happen....
+                continue;
+            }
+
+            $recipe->Name_en = $item->Name_en;
+            $recipe->Name_de = $item->Name_de;
+            $recipe->Name_fr = $item->Name_fr;
+            $recipe->Name_ja = $item->Name_ja;
+        }
+
+        GameData::saveDocument('Recipe', $document);
+    }
+}
---
+++ src/Service/Game/Pre/StainHexCodes.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Service\Game\Pre;
+
+use App\Service\Game\GameData;
+use App\Service\Github\SaintCoinach;
+use App\Service\Tools\Console;
+use App\Service\Tools\Tools;
+
+/**
+ * Add the hex value to Stain colours
+ */
+class StainHexCodes
+{
+    const ENABLED = false;
+    const ORDER = 100;
+
+    public function handle()
+    {
+        $document = GameData::loadDocument('Stain');
+
+        // add columns
+        GameData::addColumn($document, 'Hex', 'string');
+        foreach ($document->Documents as $stain) {
+            $stain->Hex = str_pad(dechex($stain->Color), 6, '0', STR_PAD_LEFT);
+        }
+
+        GameData::saveDocument('Stain', $document);
+    }
+}
